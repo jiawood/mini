f260b6c525fef7ab735f8d5b0ac5ea7daa691da3
xiaojia <xiaojia@meideng.net>
2022-03-25 22:06:01 +0800

    初步实现redux

--- redux/index.js
+++ redux/index.js
@@ -1,4 +1,4 @@
-function plan(state, action) {
+function countReducer(state, action) {
   switch (action.type) {
     case "INCREMENT":
       return {
@@ -15,7 +15,19 @@ function plan(state, action) {
   }
 }
 
-function createStore(initialState,plan) {
+function infoReducer(state, action) {
+  switch (action.type) {
+    case "SETNAME":
+      return {
+        ...state,
+        name: action.name,
+      };
+    default:
+      return state;
+  }
+}
+
+function createStore(reducer, initialState) {
   let state = initialState;
   let listeners = [];
 
@@ -23,8 +35,8 @@ function createStore(initialState,plan) {
     listeners.push(listener);
   }
 
-  function changeState(action) {
-    state = plan(state,action)
+  function dispatch(action) {
+    state = reducer(state, action);
     for (let i = 0; i < listeners.length; i++) {
       listeners[i]();
     }
@@ -36,25 +48,51 @@ function createStore(initialState,plan) {
 
   return {
     subcribe,
-    changeState,
+    dispatch,
     getState,
   };
 }
 
-const store = createStore({
-  count: 2,
-}, plan);
+const state = {
+  counter: {
+    count: 1,
+  },
+  info: {
+    name: "xj",
+    description: "前端",
+  },
+};
+
+const reducer = combineReducer({
+  counter: countReducer,
+  info: infoReducer
+})
+
+function combineReducer(reducers){
+  let keys = Object.keys(reducers)
+  return function combination(state = {}, action) {
+    const nextState = {}
+    for(let i = 0; i < keys.length; i++){
+      const key = keys[i]
+      const reducer = reducers[key]
+      const previousStateForKey = state[key]
+      const nextStateForKey = reducer(previousStateForKey,action)
+      nextState[key] = nextStateForKey
+    }
+    return nextState
+  }
+}
+
+const store = createStore(reducer, state);
+
 
 store.subcribe(() => {
   let state = store.getState();
-  console.log("info change", state.count);
+  console.log("level change", state.counter.count, state.info.name);
 });
 
-store.subcribe(() => {
-  let state = store.getState();
-  console.log("level change", state.count);
-});
+store.dispatch({ type: "INCREMENT" });
 
-store.changeState({type: 'INCREMENT'})
+store.dispatch({type:"DECREMENT"})
 
-
+store.dispatch({type:"SETNAME",name:'xiao'})
